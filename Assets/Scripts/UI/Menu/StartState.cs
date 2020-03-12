8aa32498ffde68b371612a4610a70d354d56bf8d
Ludvig Arlebrink <ludvig.arlebrink@outlook.com>
2020-03-12 15:27:20 +0100

    UI Imporvements.

--- Assets/Scripts/UI/Menu/StartState.cs
+++ Assets/Scripts/UI/Menu/StartState.cs
@@ -9,6 +9,7 @@ public class StartState : MonoBehaviourPunCallbacks
 {
     [SerializeField] private InputField m_InputField = null;
     [SerializeField] private Button m_NextButton = null;
+    [SerializeField] private Text m_InfoText = null;
     [SerializeField] private GameObject m_Connecting = null;
 
     [SerializeField] private int m_MinNameCharacters = 3;
@@ -64,6 +65,16 @@ public class StartState : MonoBehaviourPunCallbacks
     private void OnTextFieldInput(string value)
     {
         m_NextButton.interactable = m_InputField.text.Length >= m_MinNameCharacters;
+        RefreshWarnings();
+    }
+
+    private void RefreshWarnings()
+    {
+        m_InfoText.text = "";
+        if (m_InputField.text.Length < m_MinNameCharacters)
+        {
+            m_InfoText.text = "* Name must consist of 3 to 16 characters.";
+        }
     }
 
     private void Start()
@@ -75,5 +86,6 @@ public class StartState : MonoBehaviourPunCallbacks
 
         m_NextButton.interactable = m_InputField.text.Length >= m_MinNameCharacters;
         m_CanvasGroup = GetComponent<CanvasGroup>();
+        RefreshWarnings();
     }
 }
