315e44fa1259da0d2be3d71c095824aa93e5bfa4
Ludvig Arlebrink <ludvig.arlebrink@outlook.com>
2020-03-19 16:58:13 +0100

    Game logic.

--- Assets/Scripts/GameLogic/ClientSubject.cs
+++ Assets/Scripts/GameLogic/ClientSubject.cs
@@ -4,24 +4,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum ClientState
+public enum LevelIndex
 {
-    Idle,
-    LevelBank,
-    LevelBlackMarket,
-    LevelHarbour,
-    LevelMarket,
-    LevelSlum,
-    LevelJewlery,
-    LevelMuseum
+    None,
+    Bank,
+    BlackMarket,
+    Harbour,
+    Jewlery,
+    Market,
+    Museum,
+    Slum
 }
 
 [RequireComponent(typeof(PhotonView))]
 public class ClientSubject : MonoBehaviour, IPunObservable
 {
-    public GameObject m_Theif = null;
-    public ClientState m_ClientState = ClientState.Idle;
- 
+    public GameObject thief = null;
+    public LevelIndex levelIndex = LevelIndex.None;
+
+    private PathfindingAgent m_ThiefPathfindingAgent;
+    private WorldManager m_WorldManager = null;
     private PhotonView m_PhotonView = null;
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
@@ -38,30 +40,99 @@ public class ClientSubject : MonoBehaviour, IPunObservable
 
     private void ReadStream(PhotonStream stream)
     {
-        ClientState state = (ClientState)stream.ReceiveNext();
-        if (state != m_ClientState)
+        LevelIndex state = (LevelIndex)stream.ReceiveNext();
+        if (state != levelIndex)
         {
-            m_ClientState = state;
+            if (thief)
+            {
+                if (state == LevelIndex.None)
+                {
+                    thief.SetActive(false);
+
+                    // Set it in root node.
+                    thief.transform.parent = null;
+                }
+                else
+                {
+                    GameObject obj = m_WorldManager.GetLevel(state);
+
+                    if (obj)
+                    {
+                        thief.SetActive(true);
+                        thief.transform.parent = obj.transform;
+
+                    }
+                }
+
+                levelIndex = state;
+            }
         }
-
-
     }
 
     private void Start()
     {
+        m_WorldManager = FindObjectOfType<WorldManager>();
+        if (!m_WorldManager)
+        {
+            Debug.LogError("No WorldManager in scene!");
+        }
+
         m_PhotonView = GetComponent<PhotonView>();
     }
 
     private void Update()
     {
-        if (!m_PhotonView.IsMine)
+        if (!thief)
         {
+            foreach (PathfindingAgent t in FindObjectsOfType<PathfindingAgent>())
+            {
+                PhotonView photonView = t.GetComponent<PhotonView>();
+                if (photonView && m_PhotonView.OwnerActorNr == photonView.OwnerActorNr)
+                {
+                    thief = photonView.gameObject;
+                    m_ThiefPathfindingAgent = thief.GetComponent<PathfindingAgent>();
+                    if (!m_ThiefPathfindingAgent)
+                    {
+                        Debug.LogError("No pathfinding agent found in thief object.");
+                    }
+                }
+            }
+
             return;
+        }
+
+        if (m_PhotonView.IsMine)
+        {
+            if (levelIndex == LevelIndex.None)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    if (Physics.Raycast(ray, out RaycastHit hit))
+                    {
+                        m_ThiefPathfindingAgent.SetDestination(hit.point, true);
+                    }
+                }
+            }
+            else
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    if (Physics.Raycast(ray, out RaycastHit hit))
+                    {
+                        if (hit.collider.gameObject.GetComponent<StartLevel>())
+                        {
+                            // Start the level.
+                        }
+                    }
+                }
+            }
         }
     }
 
     private void WriteStream(PhotonStream stream)
     {
-        stream.SendNext(m_ClientState);
+        stream.SendNext(levelIndex);
     }
 }
---
+++ Assets/Scripts/GameLogic/StartLevel.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartLevel : MonoBehaviour
+{
+    public LevelIndex levelIndex = LevelIndex.Bank;
+}
